0861b8c90f48ca9a9144a9d549c62ee2c374421a
Brendan Bosman <bhbosman@gmail.com>
2023-03-08 12:21:18 +0200

    refactoring

--- main.go
+++ main.go
@@ -11,12 +11,14 @@ import (
 	"github.com/bhbosman/goMarketData/internal/incomingMarketData"
 	"github.com/bhbosman/goMarketData/internal/listener"
 	app2 "github.com/bhbosman/gocommon/Providers"
+	"go.uber.org/fx"
 	"net/url"
 	"os"
 )
 
 func main() {
 	u, _ := url.Parse("tcp4://127.0.0.1:4001")
+	var runTimeManager *app2.RunTimeManager
 	app := goFxApp.NewFxMainApplicationServices(
 		"MarketData",
 		false,
@@ -29,7 +31,9 @@ func main() {
 		app2.RegisterRunTimeManager(),
 		incomingMarketData.ProvideLunoMarketDataDialer(1, "tcp4://127.0.0.1:3001"),
 		incomingMarketData.ProvideKrakenMarketDataDialer(1, "tcp4://127.0.0.1:3011"),
+		fx.Populate(&runTimeManager),
 	)
+
 	if app.FxApp.Err() != nil {
 		_, _ = fmt.Fprint(os.Stderr, app.FxApp.Err().Error())
 		return
